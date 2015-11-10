326833ee2b80ed967682f1a28937c96f37694b54
benallen <external@benallen.info>
2015-11-10 03:03:49 +0000

    Semi working

--- server.js
+++ server.js
@@ -11,12 +11,33 @@ router.get('/question', function(req, res){
     res.sendFile(__dirname + '/out/checkbox_0.html');
 });
 router.get('/sendForm', function(req, res){
-    res.send("You send" + req.body.name);
-    for(var i = 0; i < 15;i++) {
+
+    var qResult = [];
+
+    for(var i = 0; i < 15; i++) {
+        var questionCorrect = true;
+
         for(var x = 0; x < 5; x++){
-            var result = req.query.input[i][x];
+            var hasValues = req.query.input.hasOwnProperty(i);
+            var shouldBe = req.query.en_input[i][x];
+
+            if(hasValues && shouldBe == "true") {
+                var result = req.query.input[i].hasOwnProperty(x);
+                if (result == false && shouldBe == "true") {
+                    questionCorrect = false;
+                }
+                if (result == true && shouldBe == "false") {
+                    questionCorrect = false;
+                }
+            } else {
+                questionCorrect = false;
+            }
         }
+        qResult.push(questionCorrect);
     }
+    console.log("hello");
+    console.log(qResult);
+    res.send("You send" + req.body.name);
 });
 router.get('/', function(req, res, next) {
     res.sendFile('../out/checkbox_0.html');
